ab0c11167c94fc4bda8631a647c8cc3266facfa7
andreaalbasini <50945937+andreaalbasini@users.noreply.github.com>
2020-01-17 00:37:57 +0100

    traduzione in inglese dei commenti nel progetto

--- AHRS/src/PrintOnScreen.h
+++ AHRS/src/PrintOnScreen.h
@@ -33,8 +33,13 @@ Includes   <System Includes> , "Project Includes"
  * Description: Stampa su schermo integrato il parametro ABS (si rimanda alla teoria AHRS).
  * 				Fornisce un indicazione della validità della precisione del magnetometro.
  * 				Valore ottimo è il più vicino possibile a 1.
+ * 				Prints parameter abs on display (ahrs theory).
+ * 				gives magnetometer precision validity indication.
+ * 				The value nearest to 1 is the optimal value.
  * Arguments: Puntatore alla struttura MAG_data
+ * 			  MAG-data struct pointer
  * Return value: Stampa sulla prima riga.
+ * 				 Prints first line
  **************************************************************************************/
 void Print_ABS(AHRS_out*);
 
@@ -42,8 +47,11 @@ void Print_ABS(AHRS_out*);
 /***************************************************************************************
  * Function name: Print_Angoli
  * Description: Stampa su schermo integrato gli angoli in gradi.
+ * 				Prints angles in degrees on display
  * Arguments: Puntatore alla struttura AHRS_data.
+ * 			  AHRS_data struct pointer
  * Return value: Stampa su righe 3, 4 e 5.
+ * 				 Prints third, forth and fifth line
  **************************************************************************************/
 void Print_Angoli(AHRS_out*);
 
@@ -51,15 +59,20 @@ void Print_Angoli(AHRS_out*);
 /***************************************************************************************
  * Function name: Print_VelAng
  * Description: Stampa su schermo integrato le velocità angolari in gradi.
+ * 				Prints angular speed in degrees on display
  * Arguments: Puntatore alla struttura AHRS_data.
+ * 			  AHRS_data struct pointer.
  * Return value: Stampa su righe 6, 7 e 8.
+ * 				 Prints sixth, seventh and eighth line.
  **************************************************************************************/
 void Print_VelAng(AHRS_out*);
 
 /***************************************************************************************
  * Function name: RealTimeChart
  * Description  : Aggiorna le variabili dichiarate globalmente, per la funzionalità di debug RealTime-Chart.
+ * 				  Debug RealTime-Chart function updates globals variables
  * Arguments    : Puntatore alla struttura AHRS_data.
+ *                AHRS_data struct pointer.
  * Return value : None.
  **************************************************************************************/
 void RealTimeChart(AHRS_out*);
@@ -67,8 +80,11 @@ void RealTimeChart(AHRS_out*);
 /***************************************************************************************
  * Print_Temp
  * Description  : Stampa su schermo integrato le velocità angolari della struttura dati imu_temp.
+ * 				  Prints imu_temp data struct angular speed on display.
  * Arguments    : Puntatore alla struttura AHRS_data.
+ * 				  AHRS_data struct pointer.
  * Return value : Stampa su righe 6,7 e 8.
+ *                Prints sixth, seventh and eighth line
  **************************************************************************************/
 void Print_Temp(AHRS_out*);
 
--- AHRS/src/mag.h
+++ AHRS/src/mag.h
@@ -42,49 +42,73 @@ typedef struct {
  * Description  : inizializza il magnetometro settando i registri per proseguire
  * 				   ed i relativi bias del magnetometro relativi al luogo di utilizzo.
  * 				  Magnetometer initialitation by registers setting to continue
- * 				  and
+ * 				  and the magnetometer relatives bias relevants to place of utilization
  * Arguments    : (MAG_data*) mag_data -puntatore alla struttura mag_data
  * 					contenente i dati relativi al magnetometro.
+ * 					mag_data struct pointer that as magnetometer data.
+ *
  * Return value : hex - 0x0 se ok.
+ * 				  hex - 0x0 if all right
  * 				  hex - 0x1 non riesce ad accedere al magnetometro 
  * 					     avvia i2c se non riesce ad avviare i2c 0x1.
+ * 				  hex - 0x1 it doesn't access to magnetometer
+ * 				        it starts i2c if it doesn't begin i2c 0x1.
  * 				  hex - 0x2 non legge il registro HMC5983_ID_A_VAL.
+ * 				  hex - 0x2 Doesn't read HMC5983_ID_A_VAL register.
  * 				  hex - 0x3 il magnetometro non e' stato identificato come
  * 							 HMC5983_ID_A_VAL come nel datasheet.
+ * 				  hex - 0x3 magnetometer hasn't identified HMC5983_ID_A_VAL as in the datasheet.
  * 				  hex - 0x4 non abilita la continuous mode HMC5983_MODE
- * 							 o non riesce a scaricare il registro HMC5983_CONF_A.
+ * 				         	o non riesce a scaricare il registro HMC5983_CONF_A.
+ * 				   hex - 0x4 doesn't enable continuous mode HMC5983_MODE or
+ * 				   doesn't download HMC5983_CONF_A register.
+ *
  * 				  hex - 0x5 non riesce a caricare il registro HMC5983_CONF_A.
+ * 				  hex - 0x5 doesn't load HMC5983_CONF_A register.
  *  			  hex - 0x6 non riesce a scaricare il registro HMC5983_CONF_B.
+ *  			  hex - 0x6 doesn't download HMC5983_CONF_B register.
  *******************************************************************************/
 int mag_init(MAG_data*);
 
 /*******************************************************************************
  * Function name: mag_read_raw
  * Description  : lettura dati grezzi (raw).
+ * 				  raw data reading.
  * Arguments    : (MAG_data*) mag_data -puntatore alla struttura mag_data
  * 					contenente i dati relativi al magnetometro.
+ * 				  (MAG_data*) mag_data -mag_data struct pointer that has magnetometer data
  * Return value : hex - 0x0 se ok.
+ * 				  hex - 0x0 if all right.
  * 				  hex - 0x1 non riesce ad accedere al registro di lettura dati 
  * 				   grezzi (raw).
+ * 				  hex - 0x1 doesn't enter to data reading register
  *******************************************************************************/
 int mag_read_raw(MAG_data*);
 
 /*******************************************************************************
  * Function name: mag_read
  * Description  : aggiustamento dati grezzi (raw) secondo il campo magnetico locale.
+ *                raw data correction according to the local magnetic field
+
  * Arguments    : (MAG_data*) mag_data -puntatore alla struttura mag_data
  * 					contenente i dati relativi al magnetometro.
+ * 				  (MAG_data*) mag_data -mag_data struct pointer that has magnetometer data.
  * Return value : hex - 0x0 se ok.
+ * 				  hex - 0x0 if all right.
  * 				  hex - 0x1 se c'e' stato un errore nella lettura dati grezzi (raw).
+ * 				  hex - 0x1 there is a raw data reading error
  *******************************************************************************/
 int mag_read(MAG_data*);
 
 /*******************************************************************************
  * Function name: mag_read
  * Description  : settaggio bias scale e abs locale.
+ * 				  Bias scale and local abs setting.
  * Arguments    : (MAG_data*) mag_data -puntatore alla struttura mag_data
  * 					contenente i dati relativi al magnetometro.
+ * 					(MAG_data*) mag_data -mag_data struct pointer that has magnetometer data.
  * Return value : int -0 se ok.
+ *                int -0 if all right.
  *******************************************************************************/
 int magcal(MAG_data*);
 
